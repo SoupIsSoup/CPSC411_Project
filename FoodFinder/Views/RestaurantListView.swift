3f5c7ec6247c14bf5e1a032b1bf7da4e047be2f6
Robert Sanchez <rsanchez51@csu.fullerton.edu>
2026-04-30 16:23:27 -0700

    Filled in ContentView and RestaurantListView

--- FoodFinder/Views/RestaurantListView.swift
+++ FoodFinder/Views/RestaurantListView.swift
@@ -1,1 +1,25 @@
+import SwiftUI
 
+struct RestaurantListView: View {
+    let restaurants: [Restaurant]
+
+    var body: some View {
+        List(restaurants) { restaurant in
+            VStack(alignment: .leading) {
+                Text(restaurant.name)
+                    .font(.headline)
+
+                if let location = restaurant.location, let city = location.city {
+                    Text(city)
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+
+                if let rating = restaurant.rating {
+                    Text("Rating: \(rating, specifier: "%.1f") ⭐")
+                        .font(.subheadline)
+                }
+            }
+        }
+    }
+}
